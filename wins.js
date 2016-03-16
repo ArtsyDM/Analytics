2c65591cd50afa1d1d164d109d469d9c905331eb
ArtsyDM <oliver.p@artsymail.com>
2016-03-16 16:37:02 -0400

    Update wins.js

--- wins.js
+++ wins.js
@@ -1,1 +1,10 @@
 console.log('hi')
+
+d3.csv("WINS.csv", function(d) {
+  return {
+    year: new Date(+d.Year, 0, 1), // convert "Year" column to Date
+    artist: d.Artist
+  };
+}, function(error, rows) {
+  console.log(rows);
+});
